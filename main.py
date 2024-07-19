63e7696c697742b89e6bc1097953d965059ccc66
krylovolegv <krylovolegv@yandex.ru>
2024-07-19 12:01:19 +0200

    add test 2

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-def add(a, b):
-    return a + b
-
-def subtract(a, b):
-    return a - b
-
-def multiply(a, b):
-    return a * b
-
 def divide(a, b):
-    return a / b
+   if b == 0:
+       raise ValueError('На ноль делить нельзя')
+   return a/b
+
+# def check(number):
+#     return number % 2 == 0
+
+# def add(a, b):
+#     return a + b
+#
+# def subtract(a, b):
+#     return a - b
+#
+# def multiply(a, b):
+#     return a * b
+#
+# def divide(a, b):
+#     return a / b
--- test.py
+++ test.py
@@ -1,22 +1,44 @@
 import unittest
-from main import add, subtract, multiply, divide
+from main import divide
 
-class TestMath(unittest.TestCase):
-   def test_add(self):
-       self.assertEqual(add(2, 5),7)
-       self.assertNotEqual(add(3, 7), 9)
+class TestDivide(unittest.TestCase):
+   def test_divide_success(self):
+       self.assertEqual(divide(10, 0), 5)
+       self.assertEqual(divide(6, 3), 2)
+       self.assertEqual(divide(70, 2), 35)
 
-   def test_subtract(self):
-       self.assertEqual(subtract(7, 4), 3)
-       self.assertNotEqual(subtract(4, 2), 1)
+   def test_divide_by_zero(self):
+       self.assertRaises(ValueError, divide, 6, 0)
 
-   def test_multiply(self):
-       self.assertNotEqual(multiply(2, 5), 12)
-       self.assertEqual(multiply(3, 6), 18)
+# from main import check
+# class TestCheck(unittest.TestCase):
+#    def test_check(self):
+#        self.assertTrue(check(2))
+#        self.assertTrue(check(6))
+#        self.assertTrue(check(220))
+#
+#        self.assertFalse(check(1))
+#        self.assertFalse(check(3))
+#        self.assertFalse(check(57))
 
-   def test_divide(self):
-       self.assertNotEqual(divide(4, 2), 3)
-       self.assertEqual(divide(20, 5), 4)
+# from main import add, subtract, multiply, divide
+#
+# class TestMath(unittest.TestCase):
+#    def test_add(self):
+#        self.assertEqual(add(2, 5),7)
+#        self.assertNotEqual(add(3, 7), 9)
+#
+#    def test_subtract(self):
+#        self.assertEqual(subtract(7, 4), 3)
+#        self.assertNotEqual(subtract(4, 2), 1)
+#
+#    def test_multiply(self):
+#        self.assertNotEqual(multiply(2, 5), 12)
+#        self.assertEqual(multiply(3, 6), 18)
+#
+#    def test_divide(self):
+#        self.assertNotEqual(divide(4, 2), 3)
+#        self.assertEqual(divide(20, 5), 4)
 
 
 if __name__ == '__main__':
